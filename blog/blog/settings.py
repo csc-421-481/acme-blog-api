d041e775071b1590b3443ef5e07c17b3fc205f25
jajorgbor <joshuaajogbor@gmail.com>
2023-12-26 09:50:33 +0100

    set debug to true

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-66-0*+l82u0$9wbx#tazg*#zy!n93!^zoj$5=^o1)#edz=@7ze"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -169,7 +169,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "/blog/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
