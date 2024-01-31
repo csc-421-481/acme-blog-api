04c8fb34a82b6f9ee03f3f9ef5a212120fb997ed
jajorgbor <joshuaajogbor@gmail.com>
2024-01-31 23:38:47 +0100

    fixed allowed cors origin error

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -91,8 +91,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3003",
     "http://localhost:3004",
     "https://acme-blog.vercel.app",
-    "https://www.modelminds.com.ng/",
-    "https://modelminds.com.ng/",
+    "https://www.modelminds.com.ng",
+    "https://modelminds.com.ng",
 ]
 
 # CORS_ALLOW_ALL_ORIGINS: True
